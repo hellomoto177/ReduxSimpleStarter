a411a3e0f6bc8aaadfe6022e1dda9f54b0e8859d
Sergey Shirokovskikh <shirokovskih_s@ekb.sima-land.ru>
2017-06-07 20:54:09 +0500

    first step

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
+import Booklist from '../containers/book-list'
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Booklist />
+            </div>
+        )
+    }
 }
